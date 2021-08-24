02d16258978c054a1708dc0cb34b00460b0c8488
Bianca <biancancanal@gmail.com>
2021-08-24 18:31:23 -0500

    Organized

---
+++ front-end/src/components/Lesson.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import LessonEdit from "../containers/LessonEdit";
+import Teacher from "../containers/Teacher";
+
+const Lesson = (props) => {
+    const [lessonEditFlag, setLessonEditFlag] = useState(false)
+
+
+    return (
+    <div>
+        <h4> {props.lesson.name} </h4>
+        {lessonEditFlag ? <LessonEdit lesson={props.lesson} toggleForm={() => setLessonEditFlag(false)} editLesson={props.editLesson}/> : <button onClick={() => setLessonEditFlag(true)}>Edit</button>}
+        <button onClick={() => props.deleteLesson(props.lesson.id)}>Delete</button>
+    </div>
+    )
+}
+
+export default Lesson;
